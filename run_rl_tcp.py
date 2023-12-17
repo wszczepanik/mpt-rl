d0afbe8d4f666939fa8d7986bacbb1b11920aacd
Wojciech Szczepanik <wojciech.szczepanik@agh.edu.pl>
2023-12-17 18:56:34 +0100

    Remove unused code

--- run_rl_tcp.py
+++ run_rl_tcp.py
@@ -34,36 +34,19 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.env_checker import check_env
 
 from custom.model import *
-from custom.wrapper import ArgumentWrapper
+from custom.wrapper import CustomMonitor
 
 logging.basicConfig(level=logging.INFO)
 
 
-def get_agent(socketUuid, useRl):
-    agent = get_agent.tcpAgents.get(socketUuid)
-    if agent is None:
-        if useRl:
-            if args.rl_algo == "DeepQ":
-                agent = TcpDeepQAgent()
-                print("new Deep Q-learning agent, uuid = {}".format(socketUuid))
-            else:
-                agent = TcpQAgent()
-                print("new Q-learning agent, uuid = {}".format(socketUuid))
-        else:
-            agent = TcpNewRenoAgent()
-            print("new New Reno agent, uuid = {}".format(socketUuid))
-        get_agent.tcpAgents[socketUuid] = agent
-
-    return agent
-
-
 if __name__ == "__main__":
-    # initialize variable
-    get_agent.tcpAgents = {}
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", type=int, help="set seed for reproducibility")
-    parser.add_argument("--sim_seed", type=int, help="set simulation run number")
+    parser.add_argument(
+        "--seed", default=42, type=int, help="set seed for reproducibility"
+    )
+    parser.add_argument(
+        "--sim_seed", default=0, type=int, help="set simulation run number"
+    )
     parser.add_argument(
         "--duration", type=float, default=10, help="set simulation duration (seconds)"
     )
@@ -81,26 +64,29 @@ if __name__ == "__main__":
         "--rl_algo", type=str, default="DeepQ", help="RL Algorithm, Q or DeepQ"
     )
     parser.add_argument(
-        "--save_dir", type=str, default="saved/", help="Location where to save data"
+        "--save_dir",
+        type=str,
+        default="scratch/reinforcement-learning/",
+        help="Location where to save data",
     )
-    parser.add_argument("--load_model", type=str, help="Load model from file, optional")
+    parser.add_argument(
+        "--load_model", default=None, type=str, help="Load model from file, optional"
+    )
+    parser.add_argument(
+        "--tensorboard_log",
+        default=None,
+        type=str,
+        help="Tensorboard log path, optional",
+    )
 
     args = parser.parse_args()
-    my_seed = 42
-    if args.seed:
-        my_seed = args.seed
+
+    my_seed = args.seed
     logging.info(f"Python side random seed {my_seed}")
     np.random.seed(my_seed)
     torch.manual_seed(my_seed)
 
-    my_sim_seed = 0
-    if args.sim_seed:
-        my_sim_seed = args.sim_seed
-
-    # if args.use_rl:
-    #     if (args.rl_algo != 'Q') and (args.rl_algo != 'DeepQ'):
-    #         print("Invalid RL Algorithm {}".format(args.rl_algo))
-    #         exit(1)
+    my_sim_seed = args.sim_seed
 
     res_list = [
         "ssThresh_l",
@@ -115,6 +101,7 @@ if __name__ == "__main__":
 
     # stepIdx = 0
 
+    # create env with env_kwargs passed as ns3 arguments
     env_kwargs = {
         "ns3Settings": {
             "transport_prot": "TcpRlTimeBased",
@@ -136,25 +123,38 @@ if __name__ == "__main__":
 
     try:
         log_path = os.path.join(args.save_dir, "log/")
-        env = ArgumentWrapper(env, env_kwargs)
+        print(log_path)
         env = Monitor(env, filename=log_path)
 
         if args.load_model:
-            model = A2C.load(args.load_model, env=env)
+            model = A2C.load(
+                args.load_model, env=env, tensorboard_log=args.tensorboard_log
+            )
         else:
-            model = A2C(CustomActorCriticPolicy, env=env, verbose=2)
+            model = A2C(
+                CustomActorCriticPolicy,
+                env=env,
+                tensorboard_log=args.tensorboard_log,
+                verbose=2,
+            )
+
+        from stable_baselines3.common.logger import configure
+
+        if args.tensorboard_log:
+            from tensorboard import program
+
+            tb = program.TensorBoard()
+            tb.configure(argv=[None, "--logdir", args.tensorboard_log])
+            url = tb.launch()
+            print(f"Tensorflow listening on {url}")
 
         logging.info(model.policy)
 
         total_timesteps = 1000
-        model.learn(total_timesteps, progress_bar=True, log_interval=10)
+        model.learn(total_timesteps, progress_bar=True, log_interval=1)
 
         # manually close env, by default it does not happen
         model.env.close()
-
-        # results_plotter.plot_results(
-        #     [log_path], total_timesteps, results_plotter.X_TIMESTEPS, "test_name"
-        # )
 
         # from stable_baselines3.common.evaluation import evaluate_policy
         # evaluate_policy(model, env, n_eval_episodes=1)
@@ -174,88 +174,3 @@ if __name__ == "__main__":
             env.close()
         except AttributeError:
             pass
-
-    # try:
-    #     obs, info = env.reset()
-    #     reward = 0
-    #     done = False
-
-    #     model = A2C(CustomActorCriticPolicy, env, verbose=2).learn(
-    #         10000, progress_bar=True
-    #     )
-    #     # model.learn
-    #     print(model.policy)
-
-    #     obs, info = env.reset()
-    #     for i in range(1000):
-    #         print(obs)
-    #         action, _state = model.predict(obs, deterministic=True)
-
-    #         print(action)
-    #         action = np.rint(action).astype(int)
-    #         # action = action.to(torch.int32)
-    #         obs, reward, terminated, _truncated, info = env.step(action)
-
-    #         model.train()
-
-    #     # while True:
-    #     #     # current ssThreshold
-    #     #     ssThresh = obs[4]
-    #     #     # current contention window size
-    #     #     cWnd = obs[5]
-    #     #     # segment size
-    #     #     segmentSize = obs[6]
-    #     #     # number of acked segments
-    #     #     segmentsAcked = obs[9]
-    #     #     # estimated bytes in flight
-    #     #     bytesInFlight = obs[7]
-
-    #     #     cur_obs = [ssThresh, cWnd, segmentsAcked, segmentSize, bytesInFlight]
-    #     #     if args.show_log:
-    #     #         logging.info("Recv obs:", cur_obs)
-
-    #     #     if args.result:
-    #     #         for res in res_list:
-    #     #             globals()[res].append(globals()[res[:-2]])
-
-    #     #     # action = tcpAgent.get_action(obs, reward, done, info)
-    #     #     action = 2
-
-    #     #     if args.show_log:
-    #     #         logging.info("Step:", stepIdx)
-    #     #         stepIdx += 1
-    #     #         logging.info("Send act:", action)
-
-    #     #     obs, reward, done, _, info = env.step(action)
-
-    #     #     if done:
-    #     #         logging.info("Simulation ended")
-    #     #         break
-
-    #     #     # get existing agent of create new TCP agent if needed
-    #     #     # tcpAgent = get_agent(obs[0], args.use_rl)
-
-    # except Exception as e:
-    #     exc_type, exc_value, exc_traceback = sys.exc_info()
-    #     logging.error("Exception occurred: {}".format(e))
-    #     logging.error("Traceback:")
-    #     traceback.print_tb(exc_traceback)
-    #     exit(1)
-
-    # else:
-    #     if args.result:
-    #         if args.result_dir:
-    #             if not os.path.exists(args.result_dir):
-    #                 os.mkdir(args.result_dir)
-    #         for res in res_list:
-    #             y = globals()[res]
-    #             x = range(len(y))
-    #             plt.clf()
-    #             plt.plot(x, y, label=res[:-2], linewidth=1, color="r")
-    #             plt.xlabel("Step Number")
-    #             plt.title("Information of {}".format(res[:-2]))
-    #             plt.savefig("{}.png".format(os.path.join(args.result_dir, res[:-2])))
-
-    # finally:
-    #     logging.info("Finally exiting...")
-    #     env.close()
