c0f6747b0d556e6bf807ed1b8db2cf2bf0f5413d
Wojciech Szczepanik <wojciech.szczepanik@agh.edu.pl>
2024-01-04 22:14:26 +0100

    Add logging observations

--- custom/callback.py
+++ custom/callback.py
@@ -5,7 +5,7 @@ class TensorboardCallback(BaseCallback):
     """Save additional data for tensorboard
 
     Args:
-        BaseCallback (_type_): _description_
+        BaseCallback (_type_): base stable baselines callback class
     """
 
     def __init__(self, verbose=1):
@@ -16,15 +16,16 @@ class TensorboardCallback(BaseCallback):
         """Run after each step, collect actions and reward
 
         Returns:
-            bool: _description_
+            bool: True for continuing
         """
         self.cum_rew += self.training_env.unwrapped.buf_rews[0]  # type: ignore
 
-        self.logger.record("train/cum_rew", self.cum_rew)
-        self.logger.record("train/ssThresh", self.training_env.unwrapped.actions[0][0])  # type: ignore
-        self.logger.record("train/cWnd", self.training_env.unwrapped.actions[0][1])  # type: ignore
+        self.logger.record("env/cum_rew", self.cum_rew)
+        self.logger.record("observation/all", self.training_env.unwrapped.buf_obs[None])
+        self.logger.record("action/ssThresh", self.training_env.unwrapped.actions[0][0])  # type: ignore
+        self.logger.record("action/cWnd", self.training_env.unwrapped.actions[0][1])  # type: ignore
 
-        # reset at episode end
+        # reset reward at episode end
         if any(self.training_env.unwrapped.buf_dones):  # type: ignore
             self.cum_rew = 0
 
