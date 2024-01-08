a81bf8c3aab8ff943636b135f2fd5fce8ad9d74f
Wojciech Szczepanik <wojciech.szczepanik@agh.edu.pl>
2024-01-08 21:28:02 +0100

    Working generator

--- custom/env_param_generator.py
+++ custom/env_param_generator.py
@@ -5,7 +5,7 @@ import re
 from pathlib import Path
 from os import PathLike
 from numbers import Number
-from typing import Dict, List, Type
+from typing import Dict, List, Tuple, Type
 
 import numpy as np
 from ruamel.yaml import YAML
@@ -24,7 +24,7 @@ class EnvParamGenerator:
     ['_bit_generator', '_poisson_lam_max', 'beta', 'binomial', 'bit_generator', 'bytes', 'chisquare', 'choice', 'dirichlet', 'exponential', 'f', 'gamma', 'geometric', 'gumbel', 'hypergeometric', 'integers', 'laplace', 'logistic', 'lognormal', 'logseries', 'multinomial', 'multivariate_hypergeometric', 'multivariate_normal', 'negative_binomial', 'noncentral_chisquare', 'noncentral_f', 'normal', 'pareto', 'permutation', 'permuted', 'poisson', 'power', 'random', 'rayleigh', 'shuffle', 'standard_cauchy', 'standard_exponential', 'standard_gamma', 'standard_normal', 'standard_t', 'triangular', 'uniform', 'vonmises', 'wald', 'weibull', 'zipf']
     """
 
-    def __init__(self, input_file: str | PathLike, target_class: Type) -> None:
+    def __init__(self, target_class: Type, input_file: str | PathLike = None) -> None:  # type: ignore
         """Constructor method
 
         Args:
@@ -36,10 +36,12 @@ class EnvParamGenerator:
 
         self.target_class = target_class
         self.target_arguments = dict()
-        self.input_file = Path(input_file)
-        self.load_yaml()
+        if input_file:
+            self.input_file = Path(input_file)
+            self._load_yaml()
+        self.rng = np.random.default_rng(0)
 
-    def load_yaml(self) -> None:
+    def _load_yaml(self) -> None:
         """Read yaml file to dict.
 
         Returns:
@@ -49,176 +51,106 @@ class EnvParamGenerator:
         self.input_dict = yaml.load(self.input_file)["ns3Settings"]
 
     def generate(self) -> Dict:
-        """Generate new QKDNetSim config and save to file.
+        """Generate new parameters dictionary.
 
         Returns:
             Dict: target class as dictionary
         """
 
-        # # kms_nodes is required, only one can exist
-        # self.output_dict["kms_nodes"] = dict()
-        # for key, value in self.input_yaml["kms_nodes"].items():
-        #     # if is number then just copy
-        #     if isinstance(value, Number):
-        #         self.output_dict["kms_nodes"][key] = value
-        #     # try generating number using numpy generator
-        #     else:
-        #         self.output_dict["kms_nodes"][key] = self.generate_value(value)
+        self.target_arguments = dict()
+        self.output = self.target_class(**self.target_arguments)
 
-        # # everything else is optional so check if it was in config
-        # if self.input_yaml["qkd_links"]:
-        #     self.output_dict["qkd_links"] = self.loop_generate_dict(
-        #         self.input_yaml["qkd_links"]
-        #     )
-
-        # # check if output is in correct format
-        # # it is simpler to check at completion
-        # try:
-        #     self.validate_output()
-        # except ValidationError as e:
-        #     logging.error(e)
-
-        self.target_arguments = self.input_dict
+        for key, value in self.input_dict.items():
+            # if is number then just copy number
+            if isinstance(value, Number):
+                self.target_arguments[key] = value
+            # check if resembles function
+            elif re.fullmatch(r"^[a-zA-Z0-9_]+\(.*\).*$", value):
+                self.target_arguments[key] = self.generate_value(value)
+            # otherwise hope that it is correct string argument
+            else:
+                self.target_arguments[key] = value
 
         self.output = self.target_class(**self.target_arguments)
 
         return self.output.asdict()
 
-    @staticmethod
-    def loop_generate_dict(self, in_dict: dict | List[dict]) -> List[dict]:
-        """Generate one type of object config using dict or list of dicts.
+    # instance method because uses self.rng object
+    def generate_value(self, func_string: str) -> int | float | str:
+        """Generate value using method string from np.random.Generator.
 
         Args:
-            in_dict (dict | List[dict]): Config dictionary or list of config dictionaries
+            func_string (str): function string
 
         Returns:
-            List[dict]: list of dictionaries with config accepted by QKDNetSim
+            int | float | str: generated scalar value
         """
-        out_dicts = list()
-        # if more than one to create (list of dicts)
-        if type(in_dict) is list:
-            for in_dict_element in in_dict:
-                out_dicts.append(dict())
-                for key, value in in_dict_element.items():
-                    # if is number then just copy
-                    if isinstance(value, Number):
-                        out_dicts[-1][key] = value
-                    # try generating number using numpy generator
-                    else:
-                        out_dicts[-1][key] = self.generate_value(value)
-        # else create one (dict)
+
+        func, args, unit = self._validate_method_string(func_string)
+        logging.debug(func, args)
+
+        if args:
+            generated = getattr(self.rng, func)(*args)
         else:
-            out_dicts.append(dict())
-            for key, value in in_dict:
-                # if is number then just copy
-                if isinstance(value, Number):
-                    out_dicts[-1][key] = value
-                # try generating number using numpy generator
-                else:
-                    out_dicts[-1][key] = self.generate_value(value)
-        return out_dicts
+            generated = getattr(self.rng, func)()
 
-    # # instance method because uses self.rng object
-    # def generate_value(self, func_string: str) -> int | float:
-    #     """Generate value using method string from np.random.Generator.
+        if isinstance(unit, str) and len(unit) > 0:
+            generated = str(generated) + unit
 
-    #     Args:
-    #         func_string (str): function string
+        return generated
 
-    #     Returns:
-    #         int | float: generated scalar value
-    #     """
+    @staticmethod
+    def _validate_method_string(m_string: str) -> Tuple[str, Tuple, str]:
+        """Check if format is correct: func(arg1,arg2)
 
-    #     # func, args = self._validate_method_string(func_string)
-    #     # logging.debug(func, args)
+        Validate if method exists and arguments are input correctly.
+        Currently only ``*args`` are accepted, ``**kwargs`` does not work.
 
-    #     # logging.debug(f"{generated=}")  # prints variable=value
+        Args:
+            m_string (str): string of method with arguments ex. ``"integers(6, 21)"``
 
-    #     # # json is not compatible with numpy types, so convert them
-    #     # # sometimes it may be interpreted as python type (like float)
-    #     # try:
-    #     #     json.dumps(generated)
-    #     #     return generated
-    #     # # sometimes it returns numpy type (like int64)
-    #     # except:
-    #     #     return generated.item()
+        Raises:
+            TypeError: method not in dir(numpy.random.Generator)
+            TypeError: wrong method format
 
-    # @staticmethod
-    # def _validate_method_string(m_string: str) -> (str, list):
-    #     """Check if format is correct: func(arg1,arg2)
-
-    #     Validate if method exists and arguments are input correctly.
-    #     Currently only ``*args`` are accepted, ``**kwargs`` does not work.
-
-    #     Args:
-    #         m_string (str): string of method with arguments ex. ``"integers(6, 21)"``
-
-    #     Raises:
-    #         TypeError: method not in dir(numpy.random.Generator)
-    #         TypeError: wrong method format
-
-    #     Returns:
-    #         str: method name
-    #         list: parsed method arguments
-    #     """
-
-    #     if re.fullmatch("[a-z]+\([a-z0-9\.\, \[\]]*\)$", m_string):
-    #         # strip from ")"
-    #         m_string = m_string[:-1]
-    #         # split function name
-    #         splitted = re.split("[\(\)]", m_string)
-    #         f_name = splitted[0]
-    #         # check if generator has this method
-    #         # todo: improve checking
-    #         method_list = [
-    #             method
-    #             for method in dir(np.random.Generator)
-    #             if method.startswith("__") is False
-    #         ]
-    #         if f_name not in method_list:
-    #             raise TypeError(f"np.random.Generator does not have {f_name}() method.")
-
-    #         # take only arguments string
-    #         splitted = splitted[1]
-    #         # convert string to arguments
-    #         splitted = ast.literal_eval(splitted)
-    #         # fix when list is treated as multiple arguments instead of one
-    #         # ex. choice([32, 64]) -> [32, 64] instead of ([32, 64],)
-    #         if type(splitted) is not tuple:
-    #             splitted = (splitted,)
-    #         return f_name, splitted
-    #     else:
-    #         raise TypeError(
-    #             f'"{m_string}" is in wrong format. Make sure it is a numpy.random.Generator method.'
-    #         )
-
-    def validate_output(self) -> None:
-        """Throw exception if field is missing or wrong type.
-
-        Output is validated using schemas from qkd_schema.
+        Returns:
+            Tuple[str, Tuple, str]: parsed function name, arguments in tuple and optional unit
         """
 
-        # data = self.output_dict
-        # # check if required field exists
-        # if "kms_nodes" in data:
-        #     # currently only one pair is supported
-        #     qkd_schema.KMSNodesModel(**data["kms_nodes"])
-        # else:
-        #     raise TypeError("kms_nodes field is required.")
-        # # check if exists
-        # if "qkd_links" in data:
-        #     # check if more than one exist
-        #     if isinstance(data["qkd_links"], list):
-        #         for link in data["qkd_links"]:
-        #             qkd_schema.QKDLinksModel(**link)
-        #     else:
-        #         qkd_schema.QKDLinksModel(data["qkd_links"])
+        if not re.fullmatch(r"^[a-z_]+\([a-z0-9\.\, \[\]]*\)[a-zA-Z]*$", m_string):
+            raise TypeError(
+                f'"{m_string}" is in wrong format. Make sure it is a numpy.random.Generator method.'
+            )
+
+        f_name, args = m_string.split("(", 1)
+        args, unit = args.rsplit(")", 1)
+
+        # delete later
+        print(f_name, args, unit)
+
+        # check if generator has this method
+        # todo: improve checking
+        method_list = [
+            method
+            for method in dir(np.random.Generator)
+            if method.startswith("__") is False
+        ]
+        if f_name not in method_list:
+            raise TypeError(f"np.random.Generator does not have {f_name}() method.")
+
+        # convert string to arguments
+        args = ast.literal_eval(args)
+        # fix when list is treated as multiple arguments instead of one
+        # ex. choice([32, 64]) -> [32, 64] instead of ([32, 64],)
+        if type(args) is not tuple:
+            args = (args,)
+        return f_name, args, unit
 
 
 if __name__ == "__main__":
     INPUT_FILE = "example.yaml"
 
-    generator = EnvParamGenerator(INPUT_FILE, TcpRlSimArgs)
-    # generator.load()
+    generator = EnvParamGenerator(TcpRlSimArgs, INPUT_FILE)
+    # seed == 0
     output = generator.generate()
     print(output)
